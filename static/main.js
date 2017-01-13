2d0a3de603a74b25185397469a564d1b86ed2755
Lori Bard <lori.r.bard@gmail.com>
2017-01-12 18:30:29 -0800

    done

---
+++ static/main.js
@@ -0,0 +1,20 @@
+console.log("Hello There Lori");
+console.log(myName);
+console.log("I love you");
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue, purple];
+
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+drawName(myName, letterColors);
+bounceBubbles();
